ac3b4ad6882734f838f1c12584e67395d271fe8c
Santiago Rodríguez <santo1996.29@gmail.com>
2020-04-20 20:48:25 -0400

    add jest for testing, add babel for using import, tests for game module

---
+++ test/game.test.js
@@ -0,0 +1,84 @@
+import '../src/game';
+
+// obviously not a pure function
+const checkWinStatus = jest.fn((status, player) => {
+  player.winner = status === player.symbol
+});
+
+// this one either, it hinges on an external object
+// actually, this function doesn't need the status at all
+const gameOver = jest.fn((playerX, playerO) => {
+  if (playerX.winner) return `${playerX.name} wins!`;
+  if (playerO.winner) return `${playerO.name} wins!`;
+  return 'It is a draw!';
+});
+
+describe('gameOver', () => {
+  const playerX = { name: 'Player X', symbol: 'x', winner: false };
+  const playerO = { name: 'Player O', symbol: 'o', winner: false };
+
+  afterEach(() => {
+    playerX.winner = false;
+    playerO.winner = false;
+  });
+
+  test('draw scenario', () => {
+    expect(gameOver(playerX, playerO)).toBe('It is a draw!');
+  });
+
+  test('x player wins', () => {
+    playerX.winner = true;
+    expect(gameOver(playerX, playerO)).toBe(`${playerX.name} wins!`);
+  });
+
+  test('o player wins', () => {
+    playerO.winner = true;
+    expect(gameOver(playerX, playerO)).toBe(`${playerO.name} wins!`);
+  });
+});
+
+describe('checkWinStatus', () => {
+  const player = { winner: false }
+
+  afterEach(() => {
+    player.winner = false;
+  });
+
+  describe('player symbol is x', () => {
+    player.symbol = 'x';
+
+    test('changes winner to true when status is x', () => {
+      checkWinStatus('x', player);
+      expect(player.winner).toBeTruthy;
+    });
+
+    test('changes nothing when status is go', () => {
+      checkWinStatus('go', player);
+      expect(player.winner).toBeFalsy;
+    });
+
+    test('changes nothing when status is o', () => {
+      checkWinStatus('o', player);
+      expect(player.winner).toBeFalsy;
+    });
+  });
+
+  describe('player symbol is o', () => {
+    player.symbol = 'o';
+
+    test('changes winner to true when status is o', () => {
+      checkWinStatus('o', player);
+      expect(player.winner).toBeTruthy;
+    });
+
+    test('changes nothing when status is go', () => {
+      checkWinStatus('go', player);
+      expect(player.winner).toBeFalsy;
+    });
+
+    test('changes nothing when status is x', () => {
+      checkWinStatus('x', player);
+      expect(player.winner).toBeFalsy;
+    });
+  });
+});
